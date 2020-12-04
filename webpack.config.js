21415180b45621f71413990fca97a2edcb2ddb21
Юрий Савенков <yura-savenkov1@yandex.ru>
2020-12-04 14:20:03 +0300

    transitions

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ const jsLoaders = () => {
 const plugins = () => {
   const base = [
     new HTMLWebpackPlugin({
-      template: './index.html',
+      template: './form.html',
       minify: {
         collapseWhitespace: isProd
       }
